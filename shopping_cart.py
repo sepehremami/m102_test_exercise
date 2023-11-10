3b1218b71e7ef67ef0ce773715235e5c6d288150
MohammadMahdiGhasemi <ghasemi.ferdosi@gmail.com>
2023-11-10 11:41:59 +0330

    fix some bugs

--- shopping_cart.py
+++ shopping_cart.py
@@ -25,36 +25,39 @@ class ShoppingCart:
         return removed_items
 
     def mean_item_price(self):
-        # Intentional mistake: Returning the item count instead of mean item price
-        return self.item_count
+        mean_item=self.total/self.item_count
+        return mean_item
 
     def median_item_price(self):
-        # Intentional mistake: Incorrect calculation of median item price
         if self.item_count == 0:
             return 0
         sorted_prices = sorted([item[1] for item in self.items])
         mid_index = self.item_count // 2
         if self.item_count % 2 == 0:
+            mid_index-=1
             return (sorted_prices[mid_index] + sorted_prices[mid_index + 1]) / 2
         else:
             return sorted_prices[mid_index]
 
     def apply_discount(self):
-        # Intentional mistake: Incorrect discount calculation
         if self.emp_discount:
-            self.total -= self.total - (self.total * self.emp_discount) / 100
+            self.base=self.total
+            self.base = self.total - ((self.total * self.emp_discount) / 100)
+        return self.base
 
     def void_last_item(self):
-        # Intentional mistake: Incorrectly reducing the item count
         if self.item_count > 0:
             last_item = self.items.pop()
             self.total -= last_item[1]
-            self.item_count -= 2
+            self.item_count -= 1
+            return self.total
+        
 
     def get_item_count(self):
         return self.item_count
 
     def get_total(self):
+        self.total=self.apply_discount
         return self.total
 
     def display_items(self):
@@ -65,3 +68,5 @@ class ShoppingCart:
         self.total = 0
         self.item_count = 0
         self.items = []
+
+

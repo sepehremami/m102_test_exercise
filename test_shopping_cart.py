2f85dd4332c7aca4f696c079539e41122a6e3b1d
ehsan <ehsan.1383asg63@yahoo.com>
2023-11-10 11:42:17 +0330

    debuging and test code

--- test_shopping_cart.py
+++ test_shopping_cart.py
@@ -1,3 +1,30 @@
 from shopping_cart import ShoppingCart
+import unittest
+shoppingcart = ShoppingCart(10)
+obj_2 = ShoppingCart(50)
+obj_2.add_item("pencile", 200)
+obj_2.add_item("book", 300)
+obj_2.add_item("pen", 400)
+obj_2.add_item("notebook", 400)
 
-# write your tests here
+obj_2.void_last_item()
+
+
+class my_test(unittest.TestCase):
+
+    def test_mean_item_price(self):
+        self.assertEqual(obj_2.mean_item_price(),
+                         obj_2.total//obj_2.item_count)
+
+    def test_median_item_price(self):
+        self.assertEqual(obj_2.median_item_price(), 350)
+
+    def test_apply_discount(self):
+        self.assertEqual(obj_2.apply_discount(), 650)
+
+    def test_void_last_item(self):
+        self.assertEqual(obj_2.void_last_item(), 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
